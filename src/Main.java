27085b632f37f6def58d62c326662fd33a565606
seyed ali <ali.tolyat@yahooc.om>
2021-04-17 05:10:30 +0430

    use dynamic programming. project works perfectly.

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,50 @@ public class Main {
         bondCode = bondCode.toLowerCase();
         int codeLen = bondCode.length();
 
-        boolean [][] validSubSt = new boolean[codeLen][codeLen];
-        for (int i = 0; i < codeLen; i++) {
-            for (int j = i; j < codeLen ; j++) {
-                validSubSt[i][j] = dictLoader.contains(bondCode.substring(i, j));
-                if (validSubSt[i][j]){
-                    System.out.print(bondCode.substring(i, j)+" ");
-                    i=j-1;
-                    break;
+        int [][] validSubSt = new int[codeLen][codeLen];
+        for (int i = 0; i < codeLen ; i++) {
+            for (int j = i+1; j <= codeLen ; j++) {
+                if (dictLoader.contains(bondCode.substring(i,j))){
+//                    System.out.println(bondCode.substring(i,j));
+                    validSubSt[i][j-1] = bondCode.substring(i,j).length();
+                }else {
+                    validSubSt[i][j-1] = 0;
                 }
             }
         }
 
+        int x = -1 ,choice = 0;
+        for (int i = 0; i < codeLen ; i++) {
+            for (int j = i+1; j < codeLen ; j++) {
+                if (validSubSt[i][j]>x){
+                    x = validSubSt[i][j];
+                    choice = j;
+                }
+            }
+            if (x!=-1 && validSubSt[i][choice]!=0){
+                System.out.print(bondCode.substring(i,choice+1)+" ");
+                i = choice;
+            }
+            x=-1;
+        }
+
+    }
+    static void findValidWords(DictionaryLoader dictLoader, int [][] validSubSt, String bondCode, int i, int j){
+        if (i==bondCode.length() || j==-1){
+            return;
+        }
+        System.out.println("i,j="+i+","+j);
+        if (dictLoader.contains(bondCode.substring(i, j))){
+            validSubSt[i][j] = bondCode.substring(i, j).length();
+            System.out.print(bondCode.substring(i, j)+" ");
+        }else {
+            validSubSt[i][j] = 0;
+            if (j>i+1){
+                findValidWords(dictLoader, validSubSt, bondCode, i+1, j);
+            }
+            if (j-1>i){
+                findValidWords(dictLoader, validSubSt, bondCode, i, j-1);
+            }
+        }
     }
 }
